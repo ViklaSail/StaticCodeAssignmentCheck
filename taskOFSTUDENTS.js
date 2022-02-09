322db5690f55973d231681f6ad9aef330d6ccb80
Zohios <andreaszohios@gmail.com>
2022-02-09 11:28:23 +0200

    Alpha Quiz_Grab

--- taskOFSTUDENTS.js
+++ taskOFSTUDENTS.js
@@ -1,23 +1,11 @@
-/**
- * Quiz/exam related functions and system level mocking functions. 
- * 
- */
+/*jshint sub:true*/
 
-var fake = require('./testVariables');
 const fs = require('fs');
 const csv = require('csv-parser');
 const csvpath = "./T42T177OJ-3001-Peräkkäisyys-periaate ja muuttujat (keskiviikkoiltaan mennessä)-TESTI.csv";
 var variable_list= [];
 var structure_list = [];
-
-function testGetQuizSubmissions(timeoutCallback){//test function simulating long lasting async
-    var fakeCheckStructureList = [];
-    fakeCheckStructureList.push(fake.taskOfStudent);
-    fakeCheckStructureList.push(fake.taskOfStudent2);
-    fakeCheckStructureList.push(fake.taskOfStudent3);
-    setTimeout(timeoutCallback, 5500, fakeCheckStructureList);
-}
-
+//var final_test = getPerson("Etunimi3", "Sukunimi3"); 
 function get_all_Students(callback) {
 
   fs.createReadStream(csvpath)
@@ -93,7 +81,7 @@ function create_object_variables(array) {
       name: array[x],
       value: variables_value
     };
-    }    
+   }    
   return variable_list;
 }
 
@@ -105,10 +93,10 @@ function create_object_stucture(ar) {
         var remove_numb = ar[x];
         ar[x] = remove_numb.split('(')[0];
       } 
-      structure_list[x] = {
-        name: ar[x],
-        value: variables_value
-      };
+     structure_list[x] = {
+       name: ar[x],
+       value: variables_value
+     };
     }    
   return structure_list;  
 }
@@ -117,7 +105,7 @@ function string_contains_number(st) {
   if (st.includes("(")) {
     return /\d/.test(st);
   } else {
-      return false;
+     return false;
 }
 }
 
@@ -131,31 +119,5 @@ function only_to_print(x) {
 function callbackAsParameter(y){
   console.log(y);
 }
- 
-module.exports = {
-    goo: "googoo",
-    getQuizSubmissions: testGetQuizSubmissions
-};
 
-/**
- * Specifications and settings for different task descriptions in the quiz. 
- * Different options for required and counted things: 
- * 
- * 1. All required reserved words/structures and variables are written in ALL CAPITALS. 
- * In task submission those can be in lower case. Reserved words are detected automatically, 
- * Rest of the detected words are function- names and variable names
- * 
- * 2. specific structure using brackes by Andreas
- * 
- * 3. First three lines in description reserved for this required words-purpose
- * first line has heading: VAADITTAVAT RAKENTEET JA NIMET nuodossa nimi:lukumääräsuositus
- * second line has required code structures and reserved words
- * third line has required variable and function names. 
- * 
- * There need to be configuration switch forcing one of these ways to search from task description. 
- * that switch can also be set to "automatic detection". That should first detect what is the 
- * method used to write required words and then use that method systematically to whole run???
- * 
- * Error handling: add to object "errors" object where you put error description as a value. 
- * 
- */
+get_all_Students(callbackAsParameter); 
