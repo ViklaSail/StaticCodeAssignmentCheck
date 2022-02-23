70af4d1ce8b9078bcb22b6852467a815ada6df50
ViklaSail <55586264+ViklaSail@users.noreply.github.com>
2022-02-23 12:16:49 +0200

    Delete palautetut directory

--- palautetut/Alina Kokko_1724154_assignsubmission_file_script.js
+++
@@ -1,7 +0,0 @@
-var nimi = "Alina";
-var day = "Monday";
-var color = "red";
-
-alert (nimi);
-alert (day);
-alert (color);
--- palautetut/Babak Rasoolnia_1724145_assignsubmission_file_scrip.js
+++
@@ -1,6 +0,0 @@
-var nimi = "Babak";
-var ika = "38";
-var ammatti = "Opiskelija";
-alert(nimi);
-alert(ika);
-alert(ammatti);
