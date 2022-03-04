d19d27f6a767814e2e8a5e6b15829d98d4d40fc1
Andreas Zohios <andreaszohios@gmail.com>
2022-03-04 15:10:35 +0200

    Source for unit debugging

--- quizModuleUnit1.test.js
+++ quizModuleUnit1.test.js
@@ -4,3 +4,5 @@ test('should output number of finnish month', () => {
     const month_number = finnishMonthConverter.finnishMonthConverter("maaliskuu");
     expect(month_number).toBe(2);
 });
+
+//https://code.visualstudio.com/docs/nodejs/nodejs-debugging
